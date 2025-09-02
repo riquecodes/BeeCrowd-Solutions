b2b045fc802644f5668f0994261bf5949e2d8281
Henrique Riva <hbriva123@gmail.com>
2025-09-01 23:55:26 -0300

    More easy level exercises

--- src/WebSearch.java
+++ src/WebSearch.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class WebSearch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        int clicksInFirstLink = t * 4;
+
+        System.out.println(clicksInFirstLink);
+    }
 }
